6a56a85cc540291a46b9c925b80d6ab3b3842024
Mohammed Shoaib <shoaibkkhn50665@gmail.com>
2024-11-17 15:57:37 +0000

    Debugged some error

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,55 +1,47 @@
-import React, { Suspense, useMemo } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Decal, OrbitControls, Preload, useTexture } from '@react-three/drei';
-import CanvasLoader from '../Loader';
+import React, {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import {Decal, Float, OrbitControls, Preload, useTexture} from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
-const Ball = React.memo(({ imgUrl }) => {
-  const [decal] = useTexture([imgUrl]);
-
-  // Use useFrame for animations instead of Float
-  useFrame(({ clock }) => {
-    const a = clock.getElapsedTime();
-    mesh.current.rotation.y = a; // Rotate the ball
-  });
-
-  const mesh = useRef();
-  
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl])
   return (
-    <mesh ref={mesh} castShadow receiveShadow scale={2.75}>
-      <icosahedronGeometry args={[1, 1]} />
-      <meshStandardMaterial
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25}/>
+      <directionalLight position={[0, 0, 0.05]}/>
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial 
         color="#80d4ff"
         polygonOffset
         polygonOffsetFactor={-5}
         flatShading
-      />
-      <Decal
+        />
+        <Decal 
         position={[0, 0, 1]}
         rotation={[2 * Math.PI, 0, 6.25]}
         flatShading
         map={decal}
-      />
-    </mesh>
-  );
-});
+        />
+      </mesh>
+    </Float>
+  )
+}
 
-const BallCanvas = ({ icon }) => {
-  const isMobile = window.innerWidth <= 768; // Adjust based on your mobile breakpoint
-
-  return (
+const BallCanvas = ({icon}) => {
+  return(
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
-      camera={{ position: [0, 0, 5], fov: isMobile ? 45 : 75 }} // Adjust camera for mobile
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={5} />
+        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={5}/>
         <Ball imgUrl={icon} />
       </Suspense>
       <Preload all />
     </Canvas>
-  );
-};
+  )
+}
 
-export default BallCanvas;
+export default BallCanvas
