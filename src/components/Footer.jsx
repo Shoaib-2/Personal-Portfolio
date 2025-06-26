bb9ae6925da223b0d4a1253bd2409226d76a7ee8
Mohammed Shoaib <shoaibkkhn50665@gmail.com>
2025-06-27 00:58:42 +0100

    Some Footer Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className='bg-primary/70 backdrop-blur-lg border-t border-white/10 text-white-100 py-4'>
       <div className='max-w-3xl mx-auto text-center'>
         <p className='text-secondary text-[16px] font-medium mb-1'>
-          &copy; {new Date().getFullYear()} Mohammed Shoaib. All rights reserved.<br />
+          &copy; {new Date().getFullYear()} Shoaib's Portfolio. All rights reserved.<br />
           Developed by Mohammed Shoaib.
         </p>
       </div>
